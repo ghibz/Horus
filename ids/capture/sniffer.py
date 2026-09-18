3da13f696caaf49595a4d5b7084ac4a39c4c4434
David Ghiburcea <114818653+ghibz@users.noreply.github.com>
2026-09-18 10:49:30 +0300

    Updated Sniffer
    updates sniffer to handle the TCP portscan and the ARP spoofing detection

--- ids/capture/sniffer.py
+++ ids/capture/sniffer.py
@@ -1,10 +1,13 @@
 from scapy.all import sniff
 from ids.capture.parser import parse_packet
+from ids.detection.portscan import process_event as process_portscan_event
+from ids.detection.arp_spoof import process_event as process_arp_event
 
 
 # Placeholder function for detection later
 def handle_packet_event(event):
-    print(event)
+    process_portscan_event(event)
+    process_arp_event(event)
 
 
 # Function called on every packet, to be parsed and handed to placeholder function
